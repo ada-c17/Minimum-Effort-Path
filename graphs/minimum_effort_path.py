9a14c2bfe8ed8f8af659fc46aa33c2d487728917
Georgia Adam <georgiaeadam@gmail.com>
2022-10-23 17:03:46 -0400

    tests passing

--- graphs/minimum_effort_path.py
+++ graphs/minimum_effort_path.py
@@ -1,3 +1,6 @@
+import heapq
+import math
+
 def min_effort_path(heights):
     """ Given a 2D array of heights, write a function to return
         the path with minimum effort.
@@ -15,4 +18,25 @@ def min_effort_path(heights):
         int
             minimum effort required to navigate the path from (0, 0) to heights[rows - 1][columns - 1]
     """
-    pass
+    if not heights:
+        return 0
+
+    m, n = len(heights), len(heights[0])
+    dist = [[math.inf] * n for _ in range(m)]
+    dist[0][0] = 0
+    minHeap = [(0, 0, 0)] # distance, row, col
+    DIR = [0, 1, 0, -1, 0]
+
+    while minHeap:
+        d, r, c = heapq.heappop(minHeap)
+        if d > dist[r][c]: continue
+        if r == m - 1 and c == n - 1:
+            return d
+        
+        for i in range(4):
+            nr, nc = r + DIR[i], c + DIR[i+1]
+            if 0 <= nr < m and 0 <= nc < n:
+                newDist = max(d, abs(heights[nr][nc] - heights[r][c]))
+                if dist[nr][nc] > newDist:
+                    dist[nr][nc] = newDist
+                    heapq.heappush(minHeap, (dist[nr][nc], nr, nc))
