983dc01217cc4d4c427a91658ddf00f09e03be0f
Thu Vuong <tavuongg@gmail.com>
2022-11-02 19:48:07 -0700

    implement dijkstras algo

--- graphs/minimum_effort_path.py
+++ graphs/minimum_effort_path.py
@@ -1,3 +1,5 @@
+import heapq
+
 def min_effort_path(heights):
     """ Given a 2D array of heights, write a function to return
         the path with minimum effort.
@@ -15,4 +17,21 @@ def min_effort_path(heights):
         int
             minimum effort required to navigate the path from (0, 0) to heights[rows - 1][columns - 1]
     """
-    pass
+    if not heights:
+        return 0
+        
+    rows, cols = len(heights), len(heights[0])
+    efforts = [[float('inf')] * cols for _ in range(rows)]
+    efforts[0][0] = 0
+    pq = [(0, 0, 0)]
+
+    while pq:
+        effort, r, c = heapq.heappop(pq)
+        if (r, c) == (rows - 1, cols - 1):
+            return effort
+        for nr, nc in (r, c + 1), (r, c - 1), (r + 1, c), (r - 1, c):
+            if 0 <= nr < rows and 0 <= nc < cols:
+                next_effort = max(effort, abs(heights[r][c] - heights[nr][nc]))
+                if efforts[nr][nc] > next_effort:
+                    efforts[nr][nc] = next_effort
+                    heapq.heappush(pq, (next_effort, nr, nc))
