1dc45c837cae1cc2618f8eeedf09931d9b168b91
Danielle <dmbecker24@gmail.com>
2022-11-05 12:12:34 -0700

    Pass all tests - use Djikstra's Theorem & Leetcode

--- graphs/minimum_effort_path.py
+++ graphs/minimum_effort_path.py
@@ -1,18 +1,26 @@
+import heapq
+import math
+
 def min_effort_path(heights):
-    """ Given a 2D array of heights, write a function to return
-        the path with minimum effort.
 
-        A route's effort is the maximum absolute difference in heights 
-        between two consecutive cells of the route.
-
-        Parameters
-        ----------
-        heights : list[list[]] (2D array)
-            2D array containing the heights of the available paths
-
-        Returns
-        -------
-        int
-            minimum effort required to navigate the path from (0, 0) to heights[rows - 1][columns - 1]
-    """
-    pass
+    if not heights:
+        return 0
+    r = len(heights)
+    c = len(heights[0])
+    diff_matrix = [[math.inf]*c for _ in range(r)]
+    diff_matrix[0][0] = 0
+    visited = [[False]*c for _ in range(r)]
+    queue = [(0, 0, 0)] 
+    while queue:
+        difference, x, y = heapq.heappop(queue)
+        visited[x][y] = True
+        for dx, dy in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
+            adj_x = x + dx
+            adj_y = y + dy
+            if 0 <= adj_x < r and 0 <= adj_y < c and not visited[adj_x][adj_y]:
+                current_difference = abs(heights[adj_x][adj_y]-heights[x][y])
+                max_difference = max(current_difference, diff_matrix[x][y])
+                if diff_matrix[adj_x][adj_y] > max_difference:
+                    diff_matrix[adj_x][adj_y] = max_difference
+                    heapq.heappush(queue, (max_difference, adj_x, adj_y))
+    return diff_matrix[-1][-1]
