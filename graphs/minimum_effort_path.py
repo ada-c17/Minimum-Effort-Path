027d9709442070a09741eb90754ede7df31049c8
Juli <juli.terapin@gmail.com>
2022-11-07 21:25:52 -0500

    Passed all tests

--- graphs/minimum_effort_path.py
+++ graphs/minimum_effort_path.py
@@ -1,3 +1,6 @@
+import math
+from heapq import heappush, heappop
+
 def min_effort_path(heights):
     """ Given a 2D array of heights, write a function to return
         the path with minimum effort.
@@ -15,4 +18,29 @@ def min_effort_path(heights):
         int
             minimum effort required to navigate the path from (0, 0) to heights[rows - 1][columns - 1]
     """
-    print('This is a test')
+    grid = heights
+
+    if not grid:
+        return 0
+        
+    h = [(0, (0,0))] 
+    costSoFar = {(0,0): 0}
+
+    dirs = [(1,0), (0,1), (-1,0), (0,-1)] 
+    trgt = (len(grid)-1, len(grid[0])-1) 
+
+    while h:
+        cost, node = heappop(h)
+        x, y = node
+        if node == trgt:
+            break
+        for dir in dirs:
+            newX, newY = x+dir[0], y+dir[1]
+
+            if newX >= 0 and newX <= len(grid)-1 and newY >= 0 and newY <= len(grid[0])-1:
+                edgeCost = max(cost, abs(grid[x][y] - grid[newX][newY])) 
+                if (newX, newY) not in costSoFar or ( (newX, newY) in costSoFar and edgeCost < costSoFar[(newX, newY)]):
+                    costSoFar[(newX, newY)] = edgeCost
+                    heappush(h, (edgeCost, (newX, newY)))
+                    
+    return costSoFar[trgt]
