7cb5392a72d3d4b62875da6d2f99ed3d374b89e2
Saria Pastor <saria.pastor@protonmail.com>
2022-11-27 19:22:35 -0500

    refactored into a SmartCyclist class

--- graphs/minimum_effort_path.py
+++ graphs/minimum_effort_path.py
@@ -1,6 +1,46 @@
 import heapq
 
-def min_effort_path(heights):
+class SmartCyclist:
+    def __init__(self, arr: list[list[int]]) -> None:
+        self.heights = arr
+        self.rows = len(arr)
+        self.columns = len(arr[0])
+        self.min_max_efforts = [[float('inf') for _ in range(self.columns)] for _ in range(self.rows)]
+        self.min_max_efforts[0][0] = 0
+    
+    def enqueue_neighbor(self, priority_queue: list[tuple[int, tuple[int, int]]], current: tuple[int, int], next: tuple[int, int], cur_max_effort: int) -> None:
+        cur_row, cur_column = current
+        next_row, next_column = next
+        max_effort = max(cur_max_effort, abs(self.heights[cur_row][cur_column] - self.heights[next_row][next_column]))
+        if max_effort < self.min_max_efforts[next_row][next_column]:
+            self.min_max_efforts[next_row][next_column] = max_effort
+        heapq.heappush(priority_queue, (self.min_max_efforts[next_row][next_column], (next_row, next_column)))
+    
+    def calculate_max_effort_along_min_effort_path(self) -> int:
+        visited = set()
+        pq = []
+        heapq.heappush(pq, (0, (0,0)))
+
+        while pq:
+            current = heapq.heappop(pq)
+            visited.add(current[1])
+            (max_effort_to_here, (row, column)) = current
+            #up
+            if row > 0 and (row - 1, column) not in visited:
+                self.enqueue_neighbor(pq, (row, column), (row - 1, column), max_effort_to_here)
+            #down
+            if row < self.rows - 1 and (row + 1, column) not in visited:
+                self.enqueue_neighbor(pq, (row, column), (row + 1, column), max_effort_to_here)
+            #left
+            if column > 0 and (row, column - 1) not in visited:
+                self.enqueue_neighbor(pq, (row, column), (row, column - 1), max_effort_to_here)
+            #right
+            if column < self.columns - 1 and (row, column + 1) not in visited:
+                self.enqueue_neighbor(pq, (row, column), (row, column + 1), max_effort_to_here)
+        return self.min_max_efforts[self.rows - 1][self.columns - 1]
+
+    
+def min_effort_path(heights: list[list[int]]):
     """ Given a 2D array of heights, write a function to return
         the path with minimum effort.
 
@@ -20,37 +60,5 @@ def min_effort_path(heights):
     if not heights:
         return 0
     
-    rows = len(heights)
-    columns = len(heights[0])
-
-    # initialize costs
-    min_max_efforts = [[float('inf') for _ in range(columns)] for _ in range(rows)]
-    min_max_efforts[0][0] = 0
-
-    visited = set()
-    pq = []
-    heapq.heappush(pq, (0, (0,0)))
-
-    while pq:
-        current = heapq.heappop(pq)
-        visited.add(current[1])
-        (max_effort_to_here, (row, column)) = current
-        #up
-        if row > 0 and (row - 1, column) not in visited:
-            visit_neighbor(heights, min_max_efforts, pq, row, column, row - 1, column, max_effort_to_here)
-        #down
-        if row < rows - 1 and (row + 1, column) not in visited:
-            visit_neighbor(heights, min_max_efforts, pq, row, column, row + 1, column, max_effort_to_here)
-        #left
-        if column > 0 and (row, column - 1) not in visited:
-            visit_neighbor(heights, min_max_efforts, pq, row, column, row, column - 1, max_effort_to_here)
-        #right
-        if column < columns - 1 and (row, column + 1) not in visited:
-            visit_neighbor(heights, min_max_efforts, pq, row, column, row, column + 1, max_effort_to_here)
-    return min_max_efforts[rows - 1][columns - 1]
-
-def visit_neighbor(heights, min_max_efforts, pq, prev_row, prev_column, next_row, next_column, max_effort_to_here):
-    max_effort = max(max_effort_to_here, abs(heights[prev_row][prev_column] - heights[next_row][next_column]))
-    if max_effort < min_max_efforts[next_row][next_column]:
-        min_max_efforts[next_row][next_column] = max_effort
-    heapq.heappush(pq, (min_max_efforts[next_row][next_column], (next_row, next_column)))
+    sc = SmartCyclist(heights)
+    return sc.calculate_max_effort_along_min_effort_path()
