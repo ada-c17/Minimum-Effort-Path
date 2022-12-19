2170531300f1ec16c23bec6ed57c3ec75c0007d5
ttienguyen <101836078+ttienguyen@users.noreply.github.com>
2022-12-18 21:41:52 -0500

    Update minimum_effort_path.py
    solution

--- graphs/minimum_effort_path.py
+++ graphs/minimum_effort_path.py
@@ -15,4 +15,43 @@ def min_effort_path(heights):
         int
             minimum effort required to navigate the path from (0, 0) to heights[rows - 1][columns - 1]
     """
-    pass
+    if (heights == None):
+        return 0
+
+    nrows = len(heights) # m
+    ncols = len(heights[0]) # n
+    dirs = [0, 1, 0, -1, 0]
+
+    # diff[i][j] gives max absolute difference to reach (i, j)
+    diff = []
+    for r in range(0, nrows):
+        new_row = []
+        for c in range(0, ncols):
+            new_row.append(math.inf)
+        diff.append(new_row)
+
+    seen = set()
+
+    minHeap = [(0, 0, 0)]  # (d, i, j)
+    diff[0][0] = 0
+
+    while minHeap:
+
+      d, i, j = heapq.heappop(minHeap)
+      if i == nrows - 1 and j == ncols - 1:
+        print(d)
+        return d
+
+      seen.add((i, j))
+      for k in range(4):
+        x = i + dirs[k]
+        y = j + dirs[k + 1]
+        if x < 0 or x == nrows or y < 0 or y == ncols:
+          continue
+        if (x, y) in seen:
+          continue
+        newDiff = abs(heights[i][j] - heights[x][y])
+        maxDiff = max(diff[i][j], newDiff)
+        if diff[x][y] > maxDiff:
+          diff[x][y] = maxDiff
+          heapq.heappush(minHeap, (diff[x][y], x, y))
