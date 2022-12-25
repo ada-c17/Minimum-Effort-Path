9233a30ce58788677e5d50e55b5603eaf3be9289
Tirhas <learningforcoding22@gmail.com>
2022-12-25 14:07:20 -0800

    All tests passed

--- graphs/minimum_effort_path.py
+++ graphs/minimum_effort_path.py
@@ -1,3 +1,9 @@
+
+import heapq
+
+# Time complexity: O(n * m * log(n * m))
+# Space complexity: O(n * m)
+
 def min_effort_path(heights):
     """ Given a 2D array of heights, write a function to return
         the path with minimum effort.
@@ -15,4 +21,31 @@ def min_effort_path(heights):
         int
             minimum effort required to navigate the path from (0, 0) to heights[rows - 1][columns - 1]
     """
-    pass
+    if not heights:
+        return 0
+    visited = set()
+    r = len(heights)
+    c = len(heights[0])
+    min_efforts = [[float('inf') for _ in range(c)] for _ in range(r)]
+    min_efforts[0][0] = 0
+    
+    pq = [(0, 0, 0)]
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    
+    while pq:
+        effort, i, j = heapq.heappop(pq)
+        if (i, j) in visited:
+            continue
+        visited.add((i, j))
+        
+        for direction in directions:
+            x = i + direction[0]
+            y = j + direction[1]
+            if 0 <= x < r and 0 <= y < c and (x, y) not in visited:
+                max_efforts = max(abs(heights[i][j] - heights[x][y]), effort)
+                if max_efforts < min_efforts[x][y]:
+                    heapq.heappush(pq, (max_efforts, x, y))
+                    min_efforts[x][y] = max_efforts
+                    
+    return min_efforts[r - 1][c - 1]
+
