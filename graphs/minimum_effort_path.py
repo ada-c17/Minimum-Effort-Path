7aff26fd25e5c652e68fd3c35d54cd009e2aef54
verasazonova <vera.zhokhova@gmail.com>
2023-01-04 00:02:32 -0800

    VeraSa C17

--- graphs/minimum_effort_path.py
+++ graphs/minimum_effort_path.py
@@ -1,3 +1,6 @@
+from collections import deque
+import heapq
+
 def min_effort_path(heights):
     """ Given a 2D array of heights, write a function to return
         the path with minimum effort.
@@ -15,4 +18,54 @@ def min_effort_path(heights):
         int
             minimum effort required to navigate the path from (0, 0) to heights[rows - 1][columns - 1]
     """
-    pass
+
+    if not heights:
+        return 0
+    
+    # rows = len(heights)
+    # columns = len(heights[0])
+    # queue = []
+    # heapq.heappush(queue, (0, 0, 0))
+    # visited = set()
+    # max_diff = 0
+    
+    # while queue:
+    #     cost, row, col = heapq.heappop(queue)
+    #     if (row, col) in visited:
+    #         continue
+    #     visited.add((row, col))
+    #     if row == rows - 1 and col == columns - 1:
+    #         return max_diff
+        
+    #     for r, c in [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]:
+    #         if r >= 0 and r < rows and c >= 0 and c < columns:
+    #             new_diff = max(max_diff, abs(heights[row][col] - heights[r][c]))
+    #             new_cost = cost + new_diff
+    #             if (r, c) not in visited or new_cost < cost:
+    #                 heapq.heappush(queue, (new_cost, r, c))
+                
+    # return max_diff
+
+    rows = len(heights)
+    columns = len(heights[0])
+    queue = []
+    heapq.heappush(queue, (0, 0, 0))
+    visited = set()
+    max_diff = 0
+    
+    while queue:
+        cost, row, col = heapq.heappop(queue)
+        max_diff = max(max_diff, cost)
+        if (row, col) in visited:
+            continue
+        visited.add((row, col))
+        if row == rows - 1 and col == columns - 1:
+            return max_diff
+        
+        for r, c in [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]:
+            if r >= 0 and r < rows and c >= 0 and c < columns and (r, c) not in visited:
+                new_diff = abs(heights[row][col] - heights[r][c])
+                heapq.heappush(queue, (new_diff, r, c))
+                
+    return max_diff
+
