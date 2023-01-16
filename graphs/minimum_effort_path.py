89b825f41271c9a2c656839068153d667abb269b
Jae <j.kerr.clayton@gmail.com>
2023-01-16 01:20:24 -0500

    reformat

--- graphs/minimum_effort_path.py
+++ graphs/minimum_effort_path.py
@@ -1,23 +1,27 @@
 from heapq import heappush, heappop
 
 def min_effort_path(heights):
-    """ Given a 2D array of heights, write a function to return
+    
+    """ 
+        Given a 2D array of heights, write a function to return
         the path with minimum effort.
 
         A route's effort is the maximum absolute difference in heights 
         between two consecutive cells of the route.
 
-        Parameters
-        ----------
-        heights : list[list[]] (2D array)
-            2D array containing the heights of the available paths
+                Parameters
+                ----------
+                heights : list[list[]] (2D array)
+                    2D array containing the heights of the available paths
 
-        Returns
-        -------
-        int
-            minimum effort required to navigate the path from (0, 0) to heights[rows - 1][columns - 1]
+                Returns
+                -------
+                int
+                minimum effort required to navigate the path from (0, 0) 
+                to heights[rows - 1][columns - 1]
+
     """
-    if heights is None: 
+    if heights is None:  
         return 0 
 
     # set target to last elem in array 
